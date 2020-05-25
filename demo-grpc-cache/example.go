90f4dfccea6e346132e3a5964f2609e9b22d1929
liuyc <ycliu912@126.com>
2020-05-25 14:44:39 +0800

    add demo-grpc-cache

---
+++ demo-grpc-cache/example.go
@@ -0,0 +1,113 @@
+package main
+
+/*
+	https://github.com/knrt10/gRPC-cache
+*/
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"strconv"
+
+	"google.golang.org/grpc"
+
+	"github.com/golang/protobuf/ptypes/empty"
+	api "github.com/knrt10/grpc-cache/proto"
+)
+
+var (
+	address string
+	conn    *grpc.ClientConn
+	err     error
+)
+
+func main() {
+
+	// Get address from flag
+	flag.StringVar(&address, "addr", ":5001", "Address on which you want to run server")
+	flag.Parse()
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err  != nil {
+		log.Fatal("did not connect : %s", err)
+	}
+	defer conn.Close()
+
+	c := api.CacheServiceClient(conn)
+
+	// Add key
+	keyVal1 := &api.Item{
+		Key:    "22",
+		Value:  "knrt10",
+		Expiration: "-1m",
+	}
+
+	keyVal2 := &api.Item{
+		Key: "2006",
+		Value: "percona",
+		Expiration: "20s",
+	}
+
+	keyVal3 := &api.Item{
+		Key: "24",
+		Value: "Palash",
+		Expiration: "2min10s",
+	}
+
+	c.Add(context.Background(), keyVal1)
+	c.Add(context.Background(), keyVal2)
+
+	addKeyRes, err := c.Add(context.Background(), keyVal3)
+	if err != nil {
+		log.Fatalf("Error from server for adding a key", addKeyRes)
+	}
+
+	// Checking for race condition
+	for i := 0; i < 50; i++ {
+		go c.Add(context.Background(), &api.Item{
+			Key: strconv.Itoa(i),
+			Value: "Value of i ls",
+			Expiration: strconv.Itoa(i),
+		})
+	}
+
+	// Get key
+	keyGet := &api.GetKey{
+		Key:    "2006",
+	}
+
+	getKeyRes, err := c.Get(context.Background(), keyGet)
+	if err != nil {
+		log.Fatalf("Error when calling Get: %s", err)
+	}
+	fmt.Println("Response from server for getting a key", getKeyRes)
+
+	// GetAllItems
+	getAllKeyRes, err := c.GetAllItems(context.Background(), *empty.Empty{})
+	if err != nil {
+		log.Fatal("Error when calling GetAllItems: %s", err)
+	}
+	fmt.Println("Response from server for getting all keys", getAllKeyRes)
+
+	// Delete Key
+	deleteKeyRes, err := c.DeleteKey(context.Background(), keyGet)
+	if err != nil {
+		log.Fatal("Error when calling DeleteKey: %s", err)
+	}
+	fmt.Println("Response from server after deleting a key", deleteKeyRes)
+
+	// DeleteAll key
+	deleteAllKeysResp, err := c.DeleteAll(context.Background(), &empty.Empty{})
+	if err != nil {
+		log.Fatal("Error when calling DeleteAll: %s", err)
+	}
+	fmt.Println("Response from server after deleting all keys", deleteAllKeysResp)
+
+	// GetAllItems after deleting key
+	_, err = c.GetAllItems(context.Background(), &empty.Empty{})
+	if err != nil {
+		fmt.Println("Response from server  after no key found", err.Error())
+	}
+}
+
---
+++ demo-grpc-cache/go.mod
@@ -0,0 +1,9 @@
+module demo.io/demo-grpc-cache
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.4.2
+	github.com/knrt10/grpc-cache v0.0.0-20200520223220-4136cb10cfd8
+	google.golang.org/grpc v1.29.1
+)
