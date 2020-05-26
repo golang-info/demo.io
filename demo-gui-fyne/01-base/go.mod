b3d7b5949a7cb3ec6c0ccb556c880bf924e2622e
liuyc <ycliu912@126.com>
2020-05-26 16:22:38 +0800

    add demo-gui-fyne

---
+++ demo-gui-fyne/01-base/go.mod
@@ -0,0 +1,5 @@
+module demo.io/demo-gui-fyne/01-base
+
+go 1.14
+
+require fyne.io/fyne v1.2.4
---
+++ demo-gui-fyne/01-base/main.go
@@ -0,0 +1,24 @@
+package main
+
+//TODO:
+// A Tour of Fyne
+// https://tour.fyne.io/basics/apprun.html
+
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	app := app.New()
+
+	w := app.NewWindow("Hello")
+	w.SetContent(widget.NewVBox(
+		widget.NewLabel("Hello Fyne!"),
+		widget.NewButton("Quit", func() {
+			app.Quit()
+		}),
+		))
+
+	w.ShowAndRun()
+}
