a2dbcc38297efe8277d7ae9fd5fddc4bd5c62ddd
liuyc <ycliu912@126.com>
2020-06-19 14:53:38 +0800

    add demo-docker

--- demo-docker/01/mian.go
+++ demo-docker/01/mian.go
@@ -10,7 +10,26 @@ import (
 func main() {
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWPID |
+			syscall.CLONE_NEWNS |
+			syscall.CLONE_NEWUSER |
+			syscall.CLONE_NEWNET,
+		UidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1,
+				HostID:      0,
+				Size:        1,
+			},
+		},
+		GidMappings: []syscall.SysProcIDMap{
+			{
+				ContainerID: 1,
+				HostID:      0,
+				Size:        1,
+			},
+		},
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
