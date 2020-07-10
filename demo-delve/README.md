c39bd893acdd9585cd1b9fb2bf317bb2d27ccde6
liuyc <ycliu912@126.com>
2020-07-10 16:34:57 +0800

    add demo-delve/

---
+++ demo-delve/README.md
@@ -0,0 +1,3 @@
+[Stop debugging Go with Println and use Delve instead](https://opensource.com/article/20/6/debug-go-delve)
+
+> 先 `break` 设置断点， 再 `continue` 运行程序至断点调试
---
+++ demo-delve/variables/variables.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+
+func main() {
+	var c, python, jave = true, false, "no!"
+	fmt.Println(i, j, c, python, jave)
+}
