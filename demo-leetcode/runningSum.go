f7930d73f2c0c266c3f079ac0beea61648503ce4
liuyc <ycliu912@126.com>
2020-07-28 16:37:35 +0800

    add demo-leetcode

---
+++ demo-leetcode/runningSum.go
@@ -0,0 +1,29 @@
+package main
+
+/*
+	https://mp.weixin.qq.com/s/fyzdzqDJPwuivo1OjKRvaw
+*/
+
+import "fmt"
+
+func main() {
+	// var demo []int
+	demo := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(runningSum(demo))
+}
+
+func runningSum(nums []int) []int {
+	length := len(nums)
+
+	res := make([]int, length)
+
+	if length < 1 {
+		return res
+	}
+
+	res[0] = nums[0]
+	for i := 1; i < length; i++ {
+		res[i] = res[i-1] + nums[i]
+	}
+	return res
+}
---
+++ demo-leetcode/twoSum.go
@@ -0,0 +1,5 @@
+package main
+
+/*
+	https://mp.weixin.qq.com/s/sTicB9IgCZxVhe53j7nsCA
+*/
