de4c209b0be5d4a300cef827d5ff08dbec27d831
ycliu912 <ycliu912@126.com>
2020-08-13 13:25:06 +0800

    add demo-time

---
+++ demo-time/timeAfter.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("You have two seconds to calculate 19 * 4")
+	for {
+		select {
+		case <-time.After(2 * time.Second):
+			fmt.Println("Time's up! The answer is 76. Did you get it?")
+		}
+	}
+}
