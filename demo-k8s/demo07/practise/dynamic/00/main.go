1255246695012639498c7901cd9527403d523f39
ycliu912 <ycliu912@126.com>
2020-09-02 10:31:13 +0800

    add demo-k8s dynamic client from official demo

--- demo-k8s/demo07/practise/dynamic/00/main.go
+++ demo-k8s/demo07/practise/dynamic/00/main.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"flag"
 	"fmt"
+	"k8s.io/client-go/util/retry"
 	"os"
 	"path/filepath"
 
+	apiv1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/runtime/schema"
@@ -85,7 +87,84 @@ func main() {
 	fmt.Printf("Created deployment %q.\n", result.GetName())
 
 	// Update Deployment
+	prompt()
+	fmt.Println("Updating deployment...")
+	// You have two options to Update() this Deployment:
+	//
+	// 1. Modify the "deployment" variable and call: Update(deployment).
+	//    This works like the "kubectl replace" command and it overwrites/loses changes
+	//    made by other clients between you Create() and Update() the object.
+	// 2. Modify the "result" returned by Get() and retry Update(result) until
+	//    you no longer get a conflict error. This way, you can preserve changes made
+	//    by other clients between Create() and Update(). This is implemented below
+	//    using the retry utility package included with client-go.(REOMMENDED)
+	//
+	//  More Info:
+	//  https://git.k8s.io/community/contributors/devel/sig-architecture/api-conventions.md#concurrency-control-and-consistency
 
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		// Retrieve the latest version of Deployment before attempting update
+		// RetryOnConflict uses exponential backoff to avoid exhausting the apiserver
+		result, getErr := client.Resource(deploymentres).Namespace(namespace).Get("demo-deployment", metav1.GetOptions{})
+		if getErr != nil {
+			panic(fmt.Errorf("failed to get latest version of Deployment: %v", getErr))
+		}
+
+		// update replicas to 1
+		if err := unstructured.SetNestedField(result.Object, int64(1), "spec", "replicas"); err != nil {
+			panic(fmt.Errorf("failed to set replica value: %v", err))
+		}
+
+		// extract spec containers
+		containers, found, err := unstructured.NestedSlice(result.Object, "spec", "template", "spec", "containers")
+		if err != nil || !found || containers == nil {
+			panic(fmt.Errorf("deployment containers not found or error in spec: %v", err))
+		}
+
+		// update container[0] image
+		if err := unstructured.SetNestedField(containers[0].(map[string]interface{}), "nginx:1.13", "image"); err != nil {
+			panic(err)
+		}
+		if err := unstructured.SetNestedField(result.Object, containers, "spec", "template", "spec", "containers"); err != nil {
+			panic(err)
+		}
+
+		_, updateErr := client.Resource(deploymentres).Namespace(namespace).Update(result, metav1.UpdateOptions{})
+		return updateErr
+	})
+	fmt.Println("Updated deployment...")
+
+	// List Deployments
+	prompt()
+	fmt.Printf("Listing deployments in namespace %q:\n", apiv1.NamespaceDefault)
+	list, err := client.Resource(deploymentres).Namespace(namespace).List(metav1.ListOptions{})
+	if err != nil {
+		panic(err)
+	}
+	for _, d := range list.Items {
+		replicas, found, err := unstructured.NestedInt64(d.Object, "spec", "replicas")
+		if err != nil || !found {
+			fmt.Printf("Replicas not found for deployment %s: error=%s", d.GetName(), err)
+		}
+		fmt.Printf(" * %s (%d replicas)\n", d.GetName(), replicas)
+	}
+
+	if retryErr != nil {
+		panic(fmt.Errorf("update failed: %v", retryErr))
+	}
+
+	// Delete Deployment
+	prompt()
+	fmt.Println("Deleting deployment...")
+	deletePolicy := metav1.DeletePropagationBackground
+	deleteOptions := metav1.DeleteOptions{
+		PropagationPolicy: &deletePolicy,
+	}
+	if err := client.Resource(deploymentres).Namespace(namespace).Delete("demo-deployment", &deleteOptions); err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Deleted deployment.")
 }
 
 func checkErr(err error) {
