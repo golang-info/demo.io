d1c97c85db28950ad8b0904797dfe3cb09e3d99d
ycliu912 <ycliu912@126.com>
2020-09-08 16:32:22 +0800

    add demo-swaggo

---
+++ demo-swaggo/00/README.md
@@ -0,0 +1,1 @@
+[使用swaggo自动生成Restful API文档](https://razeencheng.com/post/go-swagger)
---
+++ demo-swaggo/00/main.go
@@ -0,0 +1,7 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	ginSwagger "github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+)
