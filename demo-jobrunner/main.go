bc1cd5151a71197f4eed09fc9b38bfd3aba9100d
ycliu912 <ycliu912@126.com>
2020-09-21 14:46:30 +0800

    add demo jobrunner

---
+++ demo-jobrunner/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"github.com/bamzi/jobrunner"
+	"time"
+)
+
+type GreetingJob struct {
+	Name string
+}
+
+func (g GreetingJob) Run() {
+	fmt.Println("Hello, ", g.Name)
+}
+
+func main() {
+	jobrunner.Start()
+	jobrunner.Schedule("@every 5s", GreetingJob{Name: "dj"})
+
+	time.Sleep(10 * time.Second)
+}
