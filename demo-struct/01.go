85f2181b7cbc366937b49899b6a63454f2d14111
ycliu912 <ycliu912@126.com>
2020-10-23 14:56:40 +0800

    add demo-struct

---
+++ demo-struct/01.go
@@ -0,0 +1,44 @@
+package main
+
+/*
+	golang 判断结构体为空
+*/
+
+import (
+	"fmt"
+	"reflect"
+)
+
+type A struct {
+	name string
+	age  int
+}
+
+func (a A) IsEmpty() bool {
+	return reflect.DeepEqual(a, A{})
+}
+
+func main() {
+	var a A
+
+	a = A{
+		name: "ok",
+		age:  14,
+	}
+
+	if a == (A{}) {
+		fmt.Println("A == A{} empty")
+	}
+
+	if a != (A{}) {
+		fmt.Println("A == A{} not empty")
+	}
+
+	if a.IsEmpty() {
+		fmt.Println("reflect deep is empty")
+	}
+
+	if !a.IsEmpty() {
+		fmt.Println("reflect deep is not empty")
+	}
+}
